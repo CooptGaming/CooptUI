3458bf073e256d9a61e2b58be32d7d47fe6de660
Jessticy <jtlatham@gmail.com>
2026-03-24 16:57:01 -0500

    release: v0.9.6

--- lua/coopui/version.lua
+++ lua/coopui/version.lua
@@ -5,7 +5,7 @@
 
 return {
     -- Package version (e.g. for releases)
-    PACKAGE = "0.9.5",
+    PACKAGE = "0.9.6",
     -- Component versions (exposed to Lua)
     ITEMUI = "0.9.0-beta",
     SCRIPTTRACKER = "0.1.0-alpha",
